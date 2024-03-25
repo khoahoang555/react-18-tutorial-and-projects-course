4b8d0500c09a78361859e6e036f4b9c4b08a561a
khoahoang555 <khoahoangyahoo@gmail.com>
2024-03-25 08:01:41 +0700

    Update code

--- Section-3-React-Fundamentals/init-project/src/index.js
+++ Section-3-React-Fundamentals/init-project/src/index.js
@@ -1,9 +1,35 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-function Getting() {
-    return <h2>My First Component</h2>
+const BookList = () => {
+  return (
+    <section>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Image/>
+      <Title/>
+      <Author/>
+    </article>
+  )
+};
+
+const Image = () => <img
+  src="https://m.media-amazon.com/images/I/91Sy3S-198L._AC_UL320_.jpg"
+  alt=""
+/>;
+const Title = () => <h2>The Lost Bookshop: The most charming and uplifting novel for 2024 and the perfect gift for book
+  lovers!</h2>;
+const Author = () => {
+  return <h4>Evie Woods</h4>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Getting/>);
+root.render(<BookList/>);
